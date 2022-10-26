8f8f265c358bccf5d46511a1d7157caeeb1fe0f2
bre30kra69cs <an.krasowsckij@gmail.com>
2022-10-26 10:56:04 +0300

    feat: add slice

--- packages/string-tests/src/charAt.test.ts
+++ packages/string-tests/src/charAt.test.ts
@@ -4,9 +4,9 @@ describe('charAt', () => {
     expect(''.charAt(0)).toEqual('');
     expect(''.charAt(1)).toEqual('');
     expect(''.charAt(1.1)).toEqual('');
-    expect(''.charAt(2.2222)).toEqual('');
+    expect(''.charAt(2.2)).toEqual('');
     expect(''.charAt(2.6)).toEqual('');
-    expect(''.charAt(2.9999)).toEqual('');
+    expect(''.charAt(2.9)).toEqual('');
     expect(''.charAt(NaN)).toEqual('');
     expect(''.charAt(Infinity)).toEqual('');
     expect(''.charAt(-Infinity)).toEqual('');
@@ -16,9 +16,9 @@ describe('charAt', () => {
     expect('example'.charAt(-1)).toEqual('');
     expect('example'.charAt(0)).toEqual('e');
     expect('example'.charAt(1.1)).toEqual('x');
-    expect('example'.charAt(2.2222)).toEqual('a');
+    expect('example'.charAt(2.2)).toEqual('a');
     expect('example'.charAt(2.6)).toEqual('a');
-    expect('example'.charAt(2.9999)).toEqual('a');
+    expect('example'.charAt(2.9)).toEqual('a');
     expect('example'.charAt(NaN)).toEqual('e');
     expect('example'.charAt(Infinity)).toEqual('');
     expect('example'.charAt(-Infinity)).toEqual('');
